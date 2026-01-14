fef4638db792bcf8fe935b789a1e93ee7be83bff
ibrahimabdulquadri101 <ibrahimabdulquadri446@gmail.com>
2026-01-14 06:05:08 -0800

    Good validator calculator

--- main.cpp
+++ main.cpp
@@ -1,45 +1,75 @@
 #include <iostream>
+#include <cstdlib>
+#include <limits>
+
+void ignoreLine()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
 double getChar()
 {
-    std::cout << "Enter the number you like:"<<std::endl;
-    double x;
-    std::cin >> x;
-    return x;
+    while(true)
+        {
+            std::cout << "Enter your Number:";
+            double x{};
+            std::cin >> x;
+            if(std::cin.eof())
+                std::exit(0);
+            if(std::cin.fail())
+            {
+                std::cin.clear();
+                ignoreLine();
+                std::cout << "Oops, that was invalid. Try again.\n";
+            }
+            else
+            {
+                ignoreLine();
+                return x;
+            }
+        }
 }
 
-char getOperator()
+char getOp()
 {
-    std::cout << "Enter one of the following: +, -, *, or /: " <<std::endl;
-    char x;
-    std::cin >> x;
-    return x;
+    while(true)
+    {
+        std::cout << "ENTER THE OPERATION OF YOUR CHOICE: \n"; 
+        char x{};
+        std::cin >> x;
+        if(std::cin.eof())
+            std::exit(0);
+        ignoreLine();
+        if(x=='+'||x=='-'||x=='*'||x=='/')
+            return x;
+        std::cout << "Invalid operator. Please use +, -, *, or /.\n";
+    }
 }
 
 void Calculate(double x , char op , double y)
 {
-    std::cout << x << ' ' << op << ' ' << y << " is ";
-    switch (op)
-    {
-    case '+':
-        std::cout << x + y << std::endl;
-        return;
-    case '-':
-        std::cout << x - y << std::endl;
-        return;
-    case '*':
-        std::cout << x * y << std::endl;
-        return;
-    case '/':
-        std::cout << x/y  << std::endl;
-        return;
+    switch (op){
+        case '+':
+            std::cout << x + y << std::endl;
+            break;
+        case '-':
+            std::cout << x - y << std::endl;
+            break;
+        case '*':
+            std::cout << x * y << std::endl;
+            break;
+        case '/':
+            if(y==0)
+                std::cout << "Undefined (cannot divide by zero)";
+            std::cout << x / y << std::endl;
+            break;
     }
 }
 
 int main()
 {
     double x{getChar()};
-    char op{getOperator()};
+    char op{getOp()};
     double y{getChar()};
     Calculate(x,op,y);
     return 0;
